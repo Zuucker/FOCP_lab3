94b4f197cf42c3db99711b8d303652b9c43ac879
Zuucker <kamigie504@student.polsl.pl>
2020-10-22 12:23:01 +0200

    creating a struct

--- app1/app1.cpp
+++ app1/app1.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include <array>
 using namespace std;
+
+struct point2D{
+	double x;
+	double y;
+};
 
 int main()
 {
-    
+	point2D myPoint;
+	myPoint.x = 3.3;
+	myPoint.y=5.5;
+	cout << myPoint.x << " , " << myPoint.y<<endl;
+
+	point2D myOtherPoint;
+	myOtherPoint.x = 6.6;
+	myOtherPoint.y = 7.7;
+	cout << myOtherPoint.x << " , " << myOtherPoint.y<<endl;
+
+
+	return 0;
 }
